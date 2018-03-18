b9760abadcd1e20bfbc3707beae421826ab3819b
Ping Cheng <aboudicheng@gmail.com>
2018-03-18 11:18:54 +0300

    add conversions between Fahrenheit and Celsius, http to https

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var lat, long;
-var api = "http://api.openweathermap.org/data/2.5/weather?";
+var api = "https://api.openweathermap.org/data/2.5/weather?";
 var appid = "beeaf289d9cbe6bcaab2f08a8a2e0178";
 var temp;
+var unit = "°C";
 
 
 
@@ -24,10 +25,30 @@ function getWeather(lat, long) {
     method: "GET",
     url: urlStr,
     success: function(data) {
+      //get local city and country name
       $("#city").text(data.name + ", ");
       $("#country").text(data.sys.country);
+
+      //temperature initially converts from Kelvin to Celsius
       temp = data.main.temp - 273.15;
+      
+
+      //change unit when clicked
+      if (unit === "°C") {
+        $("#unit").on('click', () => {
+          unit = "°F";
+          temp = temp * 9 / 5 + 32;
+        });
+      }
+      else {
+        $("#unit").on('click', () => {
+          unit = "°C";
+          temp = (temp - 32) * 5 / 9;
+        });
+      }
+
       $("#temp").text(temp);
+      $("#unit").text(unit);
       $("#load").text("");
       console.log("Success");
     }
